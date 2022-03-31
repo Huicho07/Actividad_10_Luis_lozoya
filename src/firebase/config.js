2d6271a29b2037f287a2f0f6b087bc9b59e34116
Huicho07 <a1121250038@utch.edu.mx>
2022-03-30 18:50:22 -0700

    Actividad 10

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4KaJ-MArSxzaByKuzyaM7QU1rBTRwt78',
-  authDomain: 'actividad-10-utch.firebaseapp.com',
-  databaseURL: 'https://actividad-10-utch-default-rtdb.firebaseio.com',
-  projectId: 'actividad-10-utch',
-  storageBucket: 'actividad-10-utch.appspot.com',
-  messagingSenderId: '912457574451',
-  appId: '1:912457574451:web:27ad1971316a81d0595b46',
+  apiKey: "AIzaSyDcBNKI7VzqmD7DMI2U3dQMPtIJRVXvYV0",
+  authDomain: "actividad-10-luis-lozoya.firebaseapp.com",
+  databaseURL: "https://actividad-10-luis-lozoya-default-rtdb.firebaseio.com",
+  projectId: "actividad-10-luis-lozoya",
+  storageBucket: "actividad-10-luis-lozoya.appspot.com",
+  messagingSenderId: "787078843959",
+  appId: "1:787078843959:web:ce0c6144541fa9ec28b9b2"
 };
 
 if (!firebase.apps.length) {
